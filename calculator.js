2beb77fe38ee077a782cdb5bb6f87711d605e105
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2023-06-06 19:29:28 -0300

    created control structure for calculator

--- calculator.js
+++ calculator.js
@@ -10,10 +10,33 @@ const suma = require("./comandos/suma");
 const resta = require("./comandos/resta");
 const multiplicar = require('./comandos/multiplicar')
 
-console.log(suma(4, 7));
+/* console.log(suma(4, 7));
 console.log(resta(7, 2));
-console.log(multiplicar(3, 4));
+console.log(multiplicar(3, 4)); */
 // comandos basicos de git
 // git add. <= guarda los cambios
 // git commit -m "comentarios" <= lanza el commit, con un comentario
 // git push <= git push
+// git pull <= trae los cambios
+// git pull origin main <= trae los cambios de origin
+// git checkout nombreRama <= cambia a la rama que apunto
+
+console.log(process.argv)
+
+/* if(process.argv[2] === "suma"){
+    console.log(suma(process.argv[3], process.argv[4]))
+} else if(process.argv[2] === "resta"){
+    console.log(resta((process.argv[3], process.argv[4])))
+} */
+
+switch(process.argv[2]){
+    case "suma":
+        console.log(suma(process.argv[3], process.argv[4]))
+        break;
+    case "resta":
+        console.log(resta(process.argv[3], process.argv[4]));
+        break;
+    case "multiplicacion":
+        console.log(multiplicar((process.argv[3], process.argv[4])));
+        break;
+}
--- comandos/resta.js
+++ comandos/resta.js
@@ -1,5 +1,5 @@
 function resta(num1, num2){
-    return num1 - num2
+    return parseInt(num1, 10) - parseInt(num2, 10)
 }
 
 module.exports = resta
